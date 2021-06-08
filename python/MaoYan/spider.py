a8d4769499e3f1a2cf2835ca959bb2d8627ac6d3
wangyongwei <yeyoushen0001@163.com>
2021-06-08 18:13:34 +0800

    猫眼代码初次提交

---
+++ python/MaoYan/spider.py
@@ -0,0 +1,25 @@
+import requests
+import re
+from requests.exceptions import RequestException
+
+def get_one_page(url):
+    try:
+        headers = {
+            'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36'
+        }
+        response = requests.get(url, headers = headers)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        return None
+
+def parse_one_page(html):
+    pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name"><a'
+                         + '.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
+                         + '.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        yield {
+            'index': item[0], 
+        }
